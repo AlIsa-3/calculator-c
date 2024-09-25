3894abdc8232b918787d2c94e714f5d4f7516fcc
AlIsa-3 <182702391+AlIsa-3@users.noreply.github.com>
2024-09-25 23:18:49 +0100

    Changed calculator to use Addition and Subtraction Functions

--- calculator.c
+++ calculator.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+int addition(int first_number, int second_number);
+int subtraction(int first_number, int second_number);
+
 int main(void){
 
     int first_number;
     int second_number;
     char operation;
+    int result;
 
     printf("Enter two numbers: \n");
     scanf(" %i,%i",&first_number,&second_number);
@@ -16,14 +20,24 @@ int main(void){
 
     switch (operation){
         case '+':
-            printf("%i + %i = %i",first_number,second_number,first_number+second_number);
+            result = addition(first_number,second_number);
+            printf("%i + %i = %i",first_number,second_number,result);
             break;
         case '-':
-            printf("%i - %i = %i",first_number,second_number,first_number-second_number);
+            result = subtraction(first_number,second_number);
+            printf("%i - %i = %i",first_number,second_number,result);
             break;
         default:
             printf("Invalid Operation");
     }
 
 
+}
+
+int addition(int first_number,int second_number){
+    return first_number + second_number;
+}
+
+int subtraction(int first_number, int second_number){
+    return first_number - second_number;
 }
